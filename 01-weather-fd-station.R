2565411b45c759ca6f79561717710d82ff5d8452
Layka47 <chris47.27@proton.me>
2024-08-18 10:24:06 -0700

    Added pacman to install packages.

--- 01-weather-fd-station.R
+++ 01-weather-fd-station.R
@@ -1,2 +1,12 @@
-install.packages("tidyverse")
-library(tidyverse)
+#install.packages("pacman")
+
+# Load packages with pacman
+pacman::p_load(pacman, 
+               dplyr, 
+               ggplot2, 
+               httr, 
+               lubridate, 
+               tidyr,
+               glue,
+               viridis)
+
