e9cf75336ea69d9321808e8ba63df3c1048bdff6
Layka47 <chris47.27@proton.me>
2024-08-18 10:24:34 -0700

    Added some functions to keep the environment clean.

--- 01-weather-fd-station.R
+++ 01-weather-fd-station.R
@@ -10,3 +10,13 @@ pacman::p_load(pacman,
                glue,
                viridis)
 
+library(datasets) # Load base R packages
+
+# Clear packages
+p_unload(all)
+p_unload(viridis, lubridate) # Clear specific packages
+detach("package:datasets", unload = TRUE) # Clear base R packages
+
+# Cleanup the environment
+dev.off()   # Clear plots if there is one
+cat("\014") # Clear console. Same as Ctrl+L
