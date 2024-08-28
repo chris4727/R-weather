914daa247f8b3f5b186b231c9c8928fd6324c036
Layka47 <chris47.27@proton.me>
2024-08-28 07:58:29 -0700

    Set functions to clean environment.

--- code/01-weather-fd-station.R
+++ code/01-weather-fd-station.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 # Load packages with pacman
 pacman::p_load(pacman, 
                readr,
@@ -9,9 +11,6 @@ pacman::p_load(pacman,
                glue,
                viridis)
 
-# Cleanup the environment
-dev.off()   # Clear plots if there is one
-cat("\014") # Clear console. Same as Ctrl+L
 
 inventory_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
 
@@ -21,3 +20,7 @@ inventory <- read_table(inventory_url,
 # Tacoma latitude and longitude
 tac_lat <- 47.2526 
 tac_lon <- -122.4451
+
+# Cleanup the environment
+dev.off()   # Clear plots if there is one
+cat("\014") # Clear console. Same as Ctrl+L
