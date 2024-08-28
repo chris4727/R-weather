8743c6e807f81e6d183dd15c40f25b59d862015d
Layka47 <chris47.27@proton.me>
2024-08-28 08:57:10 -0700

    Added comments

--- code/01-weather-fd-station.R
+++ code/01-weather-fd-station.R
@@ -16,16 +16,14 @@ stations_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.t
 stations <- read_table(stations_url,
            col_names = c("station", "latitude", "longitude", "variable", "start_year", "end_year"))
 
-stations
-
 # Tacoma latitude and longitude
-my_lat <- 47.2526 * 2 * pi/360
-my_lon <- -122.4451 * 2 * pi/360
+my_lat <- 47.2526 * 2 * pi/360    # Convert latitude to radians
+my_lon <- -122.4451 * 2 * pi/360  # Convert longitude to radians
 
 my_station <- stations %>%
-  mutate(lat_rad = latitude * 2 * pi/360,
+  mutate(lat_rad = latitude * 2 * pi/360, # Convert stations lat/long to radians
          lon_rad = longitude * 2 * pi/360,
-         # 
+         # Gives distance from my_lat/my_lon in km. For miles, remove the 1.609344.
          distance = 1.609344 * 3963 * acos((sin(lat_rad) * sin(my_lat)) + cos(lat_rad) * cos(my_lat) * cos(my_lon - lon_rad))
          ) %>%  
   filter(start_year < 1988 & end_year > 2023) %>%
