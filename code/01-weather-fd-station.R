af59e2f447cad1cfcb752c016bf61246cac15d1d
Layka47 <chris47.27@proton.me>
2024-08-28 08:00:21 -0700

    Added code to find the nearest weather station.

--- code/01-weather-fd-station.R
+++ code/01-weather-fd-station.R
@@ -22,6 +22,20 @@ stations
 my_lat <- 47.2526 * 2 * pi/360
 my_lon <- -122.4451 * 2 * pi/360
 
+my_station <- stations %>%
+  mutate(lat_rad = latitude * 2 * pi/360,
+         lon_rad = longitude * 2 * pi/360,
+         # 
+         distance = 1.609344 * 3963 * acos((sin(lat_rad) * sin(my_lat)) + cos(lat_rad) * cos(my_lat) * cos(my_lon - lon_rad))
+         ) %>%  
+  filter(start_year < 1988 & end_year > 2023) %>%
+  arrange(distance) %>% 
+  top_n(n = -1, distance) %>%
+  distinct(station) #%>%
+  pull(station)
+  
+my_station
+
 # Cleanup the environment
 dev.off()   # Clear plots if there is one
 cat("\014") # Clear console. Same as Ctrl+L
